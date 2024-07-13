29b79f94156cc31317dc26b41b5e3b4c9a21c309
ronel <ronel1354@gmail.com>
2024-07-13 22:02:57 +0300

    helloworld

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,6 +1,2 @@
-def call(String repoUrl, String branch) {
-   def workingDir = "${env.WORKSPACE}"
-   bat "git clone ${repoUrl} ${workingDir}"
-   bat "git checkout ${branch}"
-   return workingDir
-}
+@Library('Shared-lib@main') _
+helloWorld()
